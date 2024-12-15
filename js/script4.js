bf4f86e637573d081f0999f77373626c8fd6357b
stefania <stefania.kovtun@yandex.ru>
2024-12-15 21:38:52 +0300

    инит комит

--- js/script4.js
+++ js/script4.js
@@ -1,3 +1,23 @@
+const textElements = document.querySelectorAll('h2');
+
+textElements.forEach(element => {
+    element.addEventListener('mouseover', () => {
+        element.style.color = 'green';
+    });
+
+    element.addEventListener('mouseout', () => {
+        element.style.color = 'black';
+    });
+});
+
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('mouseover', function() {
+        this.style.transform = 'scale(1.1)';
+    });
+    button.addEventListener('mouseout', function() {
+        this.style.transform = 'scale(1)';
+    });
+});
 function displayNotes() {
     const notesContainer = document.getElementById('notes');
     let foodEntries = JSON.parse(localStorage.getItem('foodEntries')) || [];
